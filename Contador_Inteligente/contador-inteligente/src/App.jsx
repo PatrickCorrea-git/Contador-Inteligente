dca53dc6cc499a71a158e8113a966f2473944b5c
Patrick Corrêa <patrickcs352@gmail.com>
2026-01-22 20:42:00 -0300

    commit-useEffect

--- Contador_Inteligente/contador-inteligente/src/App.jsx
+++ Contador_Inteligente/contador-inteligente/src/App.jsx
@@ -4,10 +4,20 @@ import './style.css'
 function App() {
   // Estados principais
   const [count, setCount] = useState(0);
-  const [countRender, setCountRender] = useState(0);
+  const [countRender, setRenderCount] = useState(0);
 
   // Controle da primeira render
   const isFirstRender = useRef (true);
+
+  // Efeito que reage ao contador
+  useEffect(() => {
+    if(isFirstRender.current){
+      isFirstRender.current = false;
+      return;
+    } else {
+    setRenderCount(prev => prev + 1);
+    }
+  },[count]);
 
 
   return (
