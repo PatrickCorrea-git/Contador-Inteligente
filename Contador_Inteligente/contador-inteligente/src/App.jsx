9f0f99380eaeebbb6b4d052bb171fb2b8cd59f94
Patrick Corrêa <patrickcs352@gmail.com>
2026-01-22 20:27:19 -0300

    commit-useState

--- Contador_Inteligente/contador-inteligente/src/App.jsx
+++ Contador_Inteligente/contador-inteligente/src/App.jsx
@@ -1,43 +1,17 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect } from "react";
 import './style.css'
 
 function App() {
-  // Estado do contador
+  // Estados principais
   const [count, setCount] = useState(0);
+  const [countRender, setCountRender] = useState(0);
 
-  //Referência para contar as renderizações
-  const renderCount = useRef(0);
-
-  // Controle para verificar se é a primeira renderização
-  const isFirstRender = useRef(true);
-
-  // Efeito para atualizar o controlador de renderizações
-  useEffect(() => {
-    if (isFirstRender.current) {
-      isFirstRender.current = false;
-    return;
-  } else {
-      renderCount.current += 1;
-    }
-  });
-
-  // Função para resetar os valores
-
-  const handleReset = () => {
-    setCount(0); // Resetando o contador de valores
-    renderCount.current = 0; // Resetando o contador de renderizações
-
-  };
 
   return (
     <div>
-      <h1>Contador Inteligente: {count}</h1>
-      <p>Renderizações: {renderCount.current}</p>
-      <button className='btn' onClick={() => setCount(count + 1)}>+</button>
-      <button className='btn' onClick={() => setCount(count - 1)}>-</button>
-      <button id="reset" onClick={handleReset}>Resetar</button>
+     
     </div>
   );
-};
+}
 
 export default App;
