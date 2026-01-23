a50e5de06243299966d3167a4c889c957cf4c78d
Patrick Corrêa <patrickcs352@gmail.com>
2026-01-22 21:04:25 -0300

    commit-final

--- Contador_Inteligente/contador-inteligente/src/App.jsx
+++ Contador_Inteligente/contador-inteligente/src/App.jsx
@@ -7,32 +7,32 @@ function App() {
   const [countRender, setRenderCount] = useState(0);
 
   // Controle da primeira render
-  const isFirstRender = useRef (true);
+  const isFirstRender = useRef(true);
 
   // Efeito que reage ao contador
   useEffect(() => {
     if(isFirstRender.current){
       isFirstRender.current = false;
       return;
-    } else {
-    setRenderCount(prev => prev + 1);
     }
-  },[count]);
+
+    setRenderCount(prev => prev + 1); // conta renderização após mudanças no count
+  },[count]); 
 
   // Função reset
   const handleReset = () => {
     setCount(0);
     setRenderCount(0);
-    isFirstRender = true;
+    isFirstRender.current = true;
   }
 
   return (
     <div>
      <h1>Contador Inteligente: {count}</h1>
      <p>Renderizações: {countRender}</p>
-     <button className="btn" onClick={setCount(count + 1)}>+</button>
-     <button className="btn" onClick={setCount(count - 1)}>-</button>
-     <button id="reset" onClick={handleReset}>reset</button>
+     <button className="btn" onClick={() => setCount(count + 1)}>+</button>
+     <button className="btn" onClick={() => setCount(count - 1)}>-</button>
+     <button id="reset" onClick={handleReset}>Resetar</button>
     </div>
   );
 }
